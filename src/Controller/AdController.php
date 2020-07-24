c9a1475cb15fdb5f2e30ef0e6acf7daa9188c542
yassine k <gate@LAPTOP-L7V4UEJ4.org>
2020-07-24 21:04:21 +0100

    Gestion des annonces, formulaire de creation et edition

---
+++ src/Controller/AdController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Entity\Image;
+use App\Form\AdType;
+use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ads", name="ads_index")
+     */
+    public function index(AdRepository $repo) //Injection de dependance (/ads => fournir auto un repositorie à index
+    {
+        //$repo = $this->getDoctrine()->getRepository(Ad::class);
+        $ads=$repo->findAll();
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads
+        ]);
+    }
+
+    /**
+     * Permert de creer une annonce
+     *
+     * @Route("/ads/new",name="ads_create")
+     *
+     *
+     */
+    public function create(Request $request, EntityManagerInterface $manager){
+        $ad = new Ad();
+
+        /*$image=new Image();
+        $image->setUrl('http://placehold.it/400x200')
+            ->setCaption('Titre 1');
+        $ad->addImage($image);*/
+
+        /*$form = $this->createFormBuilder($ad)
+            ->add('title')
+            ->add('introduction')
+            ->add('content')
+            ->add('rooms')
+            ->add('price')
+            ->add('coverImage')
+            ->add('save', SubmitType::class, [
+                'label' => 'Créer la nouvelle annonce',
+                'attr' => [
+                    'class' => 'btn btn-primary'
+                ]
+            ])
+            ->getForm();*/
+
+        //$request->request->get('title'); on peut recuperer les champs du formulaire de cette facon mais c un peu trop
+                        //la fct HandleResquest() permet de parcourir la requete et d'extraire les infos du form
+
+        $form=$this->createForm(AdType::class,$ad); //permet de creer un formulaire externe
+
+        //la fct handle va relie les infos recuperer à notre objet $ad créé
+        $form->handleRequest($request);
+        //dump($ad);
+        if($form->isSubmitted() && $form->isValid()) {
+            //$manager = $this->getDoctrine()->getManager();
+            foreach ($ad->getImages() as $image){
+                $image->setAd($ad);
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée ! "
+            );
+            return $this->redirectToRoute('ads_show',[ 'slug' => $ad->getSlug() ]); //crée une Response qui demande une redirection sur une autre page
+        }
+
+        return $this->render('ad/new.html.twig',[
+            "form" => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * afficher une annonce
+     *
+     * @Route("/ads/{slug}",name="ads_show")
+     */
+    public  function show(Ad $ad){ // ad dont le slug est dans la route à l'aide de ParamConverter
+        //$ad=$repo->findOneBySlug($slug);
+
+        return $this->render("ad/show.html.twig", [
+            "ad" => $ad
+        ]);
+    }
+    /*public  function show($slug,AdRepository $repo){
+        $ad=$repo->findOneBySlug($slug);
+
+        return $this->render("ad/show.html.twig", [
+            "ad" => $ad
+            ]);
+     }*/
+
+    /**
+     * Permet d'afficher le formulaire d'edition
+     * @Route("ads/{slug}/edit",name="ads_edit")
+     */
+    public function edit(Ad $ad,Request $request,EntityManagerInterface $manager){
+
+        $form=$this->createForm(AdType::class,$ad); //permet de creer un formulaire externe
+
+        //la fct handle va relie les infos recuperer à notre objet $ad créé
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            //$manager = $this->getDoctrine()->getManager();
+            foreach ($ad->getImages() as $image){
+                $image->setAd($ad);
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Les modifications de l'annonce <strong>{$ad->getTitle()}</strong> ont bien été enregistrées ! "
+            );
+            return $this->redirectToRoute('ads_show',[ 'slug' => $ad->getSlug() ]); //crée une Response qui demande une redirection sur une autre page
+        }
+
+        return $this->render("ad/edit.html.twig",[
+            "form" => $form->createView(),
+            "ad" => $ad
+        ]);
+    }
+}
---
+++ src/Form/AdType.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ad;
+use Faker\Provider\ar_JO\Text;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class AdType extends AbstractType
+{
+    /**
+     * permet d'avoir la configuration de base d'un champ !
+     *
+     * @param string $label
+     * @param string $placeholder
+     *
+     *@return array
+     */
+    private function getConfiguration($label,$placeholder,$options = []){
+        return array_merge([
+            'label'=> $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ],$options);
+    }
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+          ->add('title',TextType::class,
+              $this->getConfiguration('Titre','Taper un super titre pour votre annonce !'))
+            ->add('slug',TextType::class,
+        $this->getConfiguration('Adresse web','Taper l\'adresse web (automatique) !',['required' => false]))
+            ->add('coverImage',UrlType::class,
+                $this->getConfiguration('URL de l\'image principale','Donner une image qui donne vraiment envie !'))
+            ->add('introduction',TextType::class,
+                $this->getConfiguration('Introduction web','Donner une description globale de l\'annonce'))
+            ->add('content',TextareaType::class,
+                $this->getConfiguration('Description detaillée','Taper une description qui donne envie de venir chez vous !'))
+            ->add('rooms',IntegerType::class,
+                $this->getConfiguration('Nombre de chambre','Le nombre de chambre disponible'))
+            ->add('price',MoneyType::class,
+                $this->getConfiguration('Prix par nuit','Indiquer le prix que vous voulez pour une nuit'))
+            ->add(
+                'images',
+                CollectionType::class,// on aimerai que notre collectionType se repete qu'on va le demander=> on va donner un tableau
+                [
+                    'entry_type' => ImageType::class,    //preciser le champ ou le formulaire à repeter
+                    'allow_add' => true,    //OPTION 'ALLOW_ADD" preciser si on a le droit d'ajouter de nouveaux elements
+                    'allow_delete' => true
+                ]
+            )
+            ;
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ad::class,
+        ]);
+    }
+}
